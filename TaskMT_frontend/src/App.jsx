9bb74f592b18c005a68ec2e96d66cd38c7c4f0e1
vanessa082 <wahvanessa22@gmail.com>
2024-07-18 23:03:02 +0100

    fixed routing issue

--- TaskMT_frontend/src/App.jsx
+++ TaskMT_frontend/src/App.jsx
@@ -1,14 +1,22 @@
-
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
 import LandingPage from './Pages/landing-page/Landing-page'
 import Register from './Pages/Register/Register'
+import Footer from './Component/Common/footer'
+import Navbar from "./Component/Spercific/Navbar/Navbar";
+
 
 function App() {
-
   return (
     <>
-      <LandingPage />
+      <Navbar />
 
-      {/* <Register /> */}
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path='/registration' element={<Register />} />
+      </Routes>
+
+      <Footer />
     </>
   )
 }
