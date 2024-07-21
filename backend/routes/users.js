bfc4c59e87fd9363aa7089d5beaf064b177f5a96
vanessa082 <wahvanessa22@gmail.com>
2024-07-21 01:09:22 +0100

    wrote a condition to check if user already exist

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import registrationValidatetor from '../uitils/registrationValidator';
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// register new user
+
+router.post('/register',registrationValidatetor, function(req,res,next) {
+  const { username,email,password } = req.body;
+  try {
+    registrationValidatetor(req.body)
+    const { username,email,password } = req.body;
+  } catch (error) {
+    
+  }
+})
 
 module.exports = router;
