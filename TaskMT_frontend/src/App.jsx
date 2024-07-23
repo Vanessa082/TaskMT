7ee3e771541c77de2a359601cc8a0eef980f6b6e
vanessa082 <wahvanessa22@gmail.com>
2024-07-23 18:10:06 +0100

    merged conflict fix

--- TaskMT_frontend/src/App.jsx
+++ TaskMT_frontend/src/App.jsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom'
-import Login from './Pages/LoginPage/Login'
-import LandingPage from './Pages/landing-page/Landing-page'
-import Register from './Pages/Register/Register'
-import Footer from './Component/Common/footer'
-=======
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/landing-page/Landing-page";
 import Register from "./Pages/Register/Register";
 import Footer from "./Component/Common/footer";
->>>>>>> f0d3a27 (connected the frontend to the backend)
 import Navbar from "./Component/Spercific/Navbar/Navbar";
 
 function App() {
@@ -18,14 +10,8 @@ function App() {
       <Navbar />
 
       <Routes>
-<<<<<<< HEAD
-        <Route index path='/' element={<LandingPage />} />
-        <Route path='/registration' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-=======
         <Route index element={<LandingPage />} />
         <Route path="/registration" element={<Register />} />
->>>>>>> f0d3a27 (connected the frontend to the backend)
       </Routes>
 
       <Footer />
--- TaskMT_frontend/src/Pages/landing-page/heroSection.jsx
+++ TaskMT_frontend/src/Pages/landing-page/heroSection.jsx
@@ -2,25 +2,6 @@ import landing from "../../../public/assets/landing.svg";
 import { Link } from "react-router-dom";
 
 export default function Herosection() {
-<<<<<<< HEAD
-    return (
-        <div className="flex flex-col items-center gap-8">
-            <div className="flex flex-col text-center gap-4 " >
-                <p className="text-secondary-color font-bold text-lg pt-4">Welcome to TaskMT</p>
-                <p className="text-primary-color font-bold text-lg">Unlock Your Full Portential</p>
-            </div>
-            <img src={landing} alt="landing_icon" />
-            <div className="flex flex-col w-80 items-center">
-                <p className="text-secondary-color font-bold text-lg text-nowrap pb-8">Create track, and manage your daily tasks</p>
-                <Link to='registration'>
-                    <button className="bg-primary-color text-text-color-1 px-16 py-2 rounded-md hover:animate-bounce">Get Started</button>
-                </Link>
-            </div>
-            <hr className="text-black w-90"/>
-        </div>
-    )
-}
-=======
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-col text-center gap-4 ">
@@ -45,4 +26,3 @@ export default function Herosection() {
     </div>
   );
 }
->>>>>>> f0d3a27 (connected the frontend to the backend)
