5d18a6f350432d661f44731741402680c603bbb3
vanessa082 <wahvanessa22@gmail.com>
2024-09-02 20:18:19 +0100

    created an arrow function that will handle getting projects name therby converting the project id to to the name I passed two aguement to the funct that is the map and the the project the map was used to map the project id and equate it to the project name using the reduce methoth, method on all aprojects then returned the map the function has a sependent object

--- src/Pages/dashboard/task/independent-task/filters.jsx
+++ src/Pages/dashboard/task/independent-task/filters.jsx
@@ -1,7 +1,7 @@
 import { useDashboardContext } from "../../../../providers/context/dashboard-context";
 
 export default function TableFilters() {
-  const { tasks, setTaskQuery } = useDashboardContext();
+  const { tasks, setTaskQuery, projects } = useDashboardContext();
 
   const handleStatusChange = ({ target: { value } }) => {
     setTaskQuery((prev) => {
@@ -14,19 +14,45 @@ export default function TableFilters() {
     });
   };
 
-  // Extract unique status values for options
+  const getProjectName = projects.reduce((map, project) => {
+    map[project.id] = project.name; 
+    return map;
+  }, {});
+
+  const handleProjectChange = ({ target: { value } }) => {
+    setTaskQuery((prev) => {
+      const _prev = { ...prev };
+      if (!value) {
+        delete _prev.project_id;  
+        return _prev;
+      }
+      return { ..._prev, project_id: value }; 
+    });
+  };
+
+  const uniqueProjects = Array.from(new Set(tasks.map(task => task.project_id)));
+
   const uniqueStatuses = Array.from(new Set(tasks.map(task => task.status)));
 
   return (
     <div className="flex gap-3">
-      <select className='bg-background-color border-none flex w-28 py-1 px-4 rounded-full font-semibold'>
-        <option value="">Project</option>
-        <option value=""></option>
+      <select 
+        className='bg-background-color border-none flex w-28 py-1 px-4 rounded-full font-semibold' 
+        onChange={handleProjectChange}
+      >
+        <option value="">Projects</option>
+        {uniqueProjects.map((project_id, index) => (
+          <option key={index} value={project_id}>
+            {getProjectName[project_id]} 
+          </option>
+        ))}
       </select>
 
-      <select className='bg-background-color border-none flex w-28 py-1 px-4 rounded-full font-semibold' onChange={handleStatusChange}>
+      <select 
+        className='bg-background-color border-none flex w-28 py-1 px-4 rounded-full font-semibold' 
+        onChange={handleStatusChange}
+      >
         <option value="">Status</option>
-
         {uniqueStatuses.map((status, index) => (
           <option key={index} value={status}>{status}</option>
         ))}
